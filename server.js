4ed1d7be6ab98dc61b5283948821b34077c882c1
rahul-5503 <rahulkrishna5503@gmail.com>
2024-01-07 07:55:58 +0530

    server three  update

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const http = require('http');
-const app = require('./app');
-
+const express = require('express');
+const app=express();
+const morgan =require('morgan');
+const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv');
+dotenv.config();
+const productroutes=require('./api/routes/products');
+const ordersroutes=require('./api/routes/Orders');
+  
+
+
 const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
 
 mongoose.set('strictQuery',false);
 const connectDB = async ()=>{
@@ -15,15 +19,32 @@ const connectDB = async ()=>{
         console.log(`MongoDB Connection: ${conn.connection.host}`);
     }
     catch(error){
-        console.log(error);
+        console.log('mongodb connection error:',error);
         process.exit(1);
     }
 }
-mongoose.connect(
-    process.env.MONGODBURL
-  );
-  
+
   connectDB().then(()=>{
-app.listen(3000,()=>{
+app.listen(port,()=>{
   console.log(`Server running on port ${port}`)})
+})
+
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+app.use('/uploads',express.static('uploads'));
+app.use('/products',productroutes);
+app.use('/orders',ordersroutes);
+    
+app.use((req,res,next)=>{
+    const error=new Error('Not found');
+    error.status=404;
+    next(error);
+});
+app.use((error,req,res,next)=>{
+    res.status(error.status||502);
+    res.json({
+        error:{
+            message:error.message
+        }})
 })
