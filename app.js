2db0a64ef53687243a8d3f96674fefafdb891243
rahul-5503 <rahulkrishna5503@gmail.com>
2024-01-07 08:05:00 +0530

    server /get  update

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const ordersroutes=require('./api/routes/Orders');
 app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
+app.get('/', (req, res) => {
+    res.send('Welcome to the server!'); // Or any other appropriate response
+});
+
 app.use('/uploads',express.static('uploads'));
 app.use('/products',productroutes);
 app.use('/orders',ordersroutes);
